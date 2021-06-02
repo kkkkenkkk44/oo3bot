b71b047523a8858149b600d9e0b2b10be6c7645d
楊鈞智 <ken@yangjunzhide-MacBook-Pro.local>
2021-06-02 17:14:32 +0800

    add some things

---
+++ oo3.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require("./config.json");
+
+// 連上線時的事件
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+// 當 Bot 接收到訊息時的事件
+client.on('message', msg => {
+
+    if (msg.content === '那我也要睡啦') {
+
+        msg.reply('好啊快睡！');
+    }
+});
+
+client.login(config.token);
